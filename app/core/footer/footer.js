0a61dabab5b2f02b301e9e362dd8354339fba377
Aliaksandr Hizels <Aliaksandr_Hizels@epam.com>
2019-10-24 16:08:47 +0300

    code refactoring footer

--- app/core/footer/footer.js
+++ app/core/footer/footer.js
@@ -1,13 +1,10 @@
-var footer = angular.module('myApp.footer',['footerDirective']);
+angular.module('myApp.footer', ['footerDirective']);
 
-angular.module('footerDirective',[])
-.directive('appFooter', function () {
-  return {
-    scope: false,
-    templateUrl: "/core/footer/tmp/footer.tmp.html",
-    link: function (scope, element, attrs) {
-      scope.data = scope[attrs["title"]];
-    },
-    restrict: "E"
-  };
+angular.module('footerDirective', [])
+    .directive('appFooter', function () {
+        return {
+            scope: false,
+            templateUrl: "/core/footer/tmp/footer.tmp.html",
+            restrict: "E"
+        };
 });
